463749b20fd5e9b62ca4a882b46b080801189d2c
Felipeb26 <felipeb2silva@hotmail.com>
2022-07-10 00:28:52 -0300

    deploy heroku

--- app.js
+++ app.js
@@ -1,13 +1,10 @@
-const jsonServer = require('json-server')
-
+const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000;
 
-const PORT = process.env.PORT || 3001;
+server.use(middlewares);
+server.use(router);
 
-server.use(middlewares)
-server.use(router)
-server.listen(PORT, () =>{
-    console.log("server running under port: ", PORT);
-})
+server.listen(port);
